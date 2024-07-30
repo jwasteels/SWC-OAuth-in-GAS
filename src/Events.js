6a9ae6dcd07ba6ad08d8cef37f4b534e1cdabb09
jwasteels <jeroenwasteels@gmail.com>
2024-07-30 15:52:49 +0200

    Update Events.js

--- src/Events.js
+++ src/Events.js
@@ -4,13 +4,19 @@ function onOpen() {
       ui.createMenu('🚀SWC API')
       .addSubMenu(ui.createMenu('New users')
       .addItem('New User Authentication','getAuthorization')
-      .addItem('New User Authentication With Extra Scope','exampleAuthorization')
+      
           )
       .addSeparator()
       .addSubMenu(ui.createMenu('Single user operations')
         .addItem('Popup active user','popupActiveUser')
         .addItem('Display active user profile', 'loadActiveProfile')
         .addItem('Clear active user profile', 'clearActiveProfile')
+          )
+      .addSeparator()
+      .addSubMenu(ui.createMenu('Examples')
+        .addItem('New User Authentication With Extra Scope','exampleAuthorization')
+        .addItem('Show a ship from your inventory', 'exampleReadShip')
+        .addItem('Rename a ship from your inventory', 'exampleWriteShip')
           )
       .addToUi();
 }
