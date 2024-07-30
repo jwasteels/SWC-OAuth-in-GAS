00d3fe3b20925ec9747a6cadfd3b274f02be429c
jwasteels <jeroenwasteels@gmail.com>
2024-07-30 16:30:13 +0200

    Update OAuth2_SWC.js
    Move secret constants out

--- src/API/OAuth2_SWC.js
+++ src/API/OAuth2_SWC.js
@@ -1,10 +1,4 @@
-/**
- * The SWC OAuth2 client ID and secret, and other constants
- */
- const redirect_uri = 'XXX'; //Replace with the output of getRedirectUri
- const CLIENT_ID = 'XXX'; //Replace with your client ID
- const CLIENT_SECRET = 'XXX'; //Replace with your client secret
- const default_user = 'XXX'; //Replace with a default user if applicable, or blank
+
  const expirationDelay = 60; //This is what is used by the SWC code
  const authorizationBaseUrl = 'https://www.swcombine.com/ws/oauth2/auth/'
  const tokenBaseUrl = 'https://www.swcombine.com/ws/oauth2/token/'
