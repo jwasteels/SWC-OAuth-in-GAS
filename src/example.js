c5b2d272fd398733590a1abd61d6bab7b03e61f0
jwasteels <jeroenwasteels@gmail.com>
2024-07-30 15:51:08 +0200

    Update example.js

--- src/example.js
+++ src/example.js
@@ -1,10 +1,20 @@
-
-
-
-//Building a scope
+function exampleAuthorization {
   var scope = []
   scope.push('CHARACTER_READ');
-  scope.push('FACTION_INV_OVERVIEW');
-  scope.push('FACTION_INV_MATERIALS_READ');
-  scope.push('FACTION_INV_MATERIALS_TAGS_READ');
+  scope.push('PERSONAL_INV_OVERVIEW');
+  scope.push('PERSONAL_INV_SHIPS_READ');
+  scope.push('PERSONAL_INV_SHIPS_WRITE');
 
+  getAuthorization(scope);
+}
+
+function exampleReadShip {
+
+}
+
+function exampleWriteShip {
+  var newName = 'This ship was renamed through the SWC Oauth in GAS example script';
+  
+
+
+}
