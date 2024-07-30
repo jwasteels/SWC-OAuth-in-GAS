f7cb3da2a9aa2b22b30d38f4793b8034522bcbcd
jwasteels <jeroenwasteels@gmail.com>
2024-07-30 15:16:45 +0200

    Create example.js

---
+++ src/example.js
@@ -0,0 +1,10 @@
+
+
+
+//Building a scope
+  var scope = []
+  scope.push('CHARACTER_READ');
+  scope.push('FACTION_INV_OVERVIEW');
+  scope.push('FACTION_INV_MATERIALS_READ');
+  scope.push('FACTION_INV_MATERIALS_TAGS_READ');
+
