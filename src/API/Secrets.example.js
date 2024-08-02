ea38af22f0d9cc5c86277b977476943ac2ff62ed
Jeroen Wasteels <jeroenwasteels@gmail.com>
2024-08-02 11:54:18 +0200

    Ignore actual config file, add example file

---
+++ src/API/Secrets.example.js
@@ -0,0 +1,7 @@
+/**
+ * The SWC OAuth2 client ID and secret, and other constants
+ */
+const redirect_uri = 'XXX'; //Replace with the output of getRedirectUri
+const CLIENT_ID = 'XXX'; //Replace with your client ID
+const CLIENT_SECRET = 'XXX'; //Replace with your client secret
+const default_user = 'XXX'; //Replace with a default user if applicable, or blank
